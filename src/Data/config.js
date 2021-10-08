909633b55f717fd5e1054e4321f9fbdac69ae8e2
José Lucas <josluc2016@gmail.com>
2021-10-08 19:18:11 -0300

    feat: some more functions had been added

--- src/Data/config.js
+++ src/Data/config.js
@@ -1,54 +1,183 @@
 export const details = {
     nfc:{
         west:{
-            one: "Arizona Cardinals",
-            two: "Los Angeles Rams",
-            three: "San Francisco 49ers",
-            four: "Seattle Seahawks"
+            one: {
+                name: "Arizona Cardinals",
+                color1: '#FFB612',
+                color2: '#97233F'
+            },
+            two: {
+                name: "Los Angeles Rams",
+                color1: '#003594',
+                color2: '#FFA300'
+            },
+            three: {
+                name:"San Francisco 49ers",
+                color1: '#AA0000',
+                color2: '#B3995D'
+            },
+            four: {
+                name: "Seattle Seahawks",
+                color1: '#002244',
+                color2: '#69BE28'
+            }
         },
         north:{
-            one: "Green Bay Packers",
-            two: "Chicago Bears",
-            three: "Minnesota Vikings",
-            four: "Detroit Lions"
+            one: {
+                name: "Green Bay Packers",
+                color1: 'darkgreen',
+                color2: '#FFB612'
+            },
+            two: {
+                name: "Chicago Bears",
+                color2: '#0B162A',
+                color1: '#C83803'
+            },
+            three: {
+                name: "Minnesota Vikings",
+                color2: '#4F2683',
+                color1: '#FFC62F'
+            },
+            four: {
+                name: "Detroit Lions",
+                color1: '#0076B6',
+                color2: '#B0B7BC'
+            }
         },
         south:{
-            one: "Tampa Bay Buccaneers",
-            two: "New Orleans Saints",
-            three: "Carolina Panthers",
-            four: "Atlanta Falcons"
+            one: {
+                name: "Tampa Bay Buccaneers",
+                color2: '#D50A0A',
+                color1: '#0A0A08'
+            },
+            two: {
+                name: "New Orleans Saints",
+                color2: '#D3BC8D',
+                color1: '#101820'
+            },
+            three: {
+                name: "Carolina Panthers",
+                color2: '#0085CA',
+                color1: '#101820'
+            },
+            four: {
+                name: "Atlanta Falcons",
+                color2: '#A71930',
+                color1: '#000000'
+            }
         },
         east:{
-            one: "Philadelphia Eagles",
-            two: "Dallas Cowboys",
-            three: "New York Giants",
-            four: "Washington Football Team"
+            one: {
+                name: "Philadelphia Eagles",
+                color1: '#004C54',
+                color2: '#A5ACAF'
+            },
+            two: {
+                name: "Dallas Cowboys",
+                color2: '#003594',
+                color1: '#A5ACAF'
+            },
+            three: {
+                name: "New York Giants",
+                color1: '#0B2265',
+                color2: '#A71930',
+                shadow: 'white'
+            },
+            four: {
+                name: "Washington Football Team",
+                color1: '#773141',
+                color2: '#FFB612'
+            }
         }
     },
     afc:{
         south:{
-            one: "Tennessee Titans",
-            two: "Houston Texans",
-            three: "Indianapolis Colts",
-            four: "Jacksonville Jaguars"
+            one: {
+                name: "Tennessee Titans",
+                color1: 'blue',
+                color2: 'skyblue'
+            },
+            two: {
+                name: "Houston Texans",
+                color1: 'blue',
+                color2: 'red'
+            },
+            three: {
+                name: "Indianapolis Colts",
+                color1: 'blue',
+                color2: 'white'
+            },
+            four: {
+                name: "Jacksonville Jaguars",
+                color1: 'yellow',
+                color2: 'green'
+            }
         },
         west:{
-            one: "Los Angeles Chargers",
-            two: "Las Vegas Raiders",
-            three: "Kansas City Chiefs",
-            four: "Denver Broncos"
+            one: {
+                name:"Los Angeles Chargers",
+                color1: 'yellow',
+                color2: 'skyblue'
+            },
+            two: {
+                name: "Las Vegas Raiders",
+                color1: 'white',
+                color2: 'black'
+            },
+            three: {
+                name: "Kansas City Chiefs",
+                color1: 'red',
+                color2: 'white'
+            },
+            four: {
+                name: "Denver Broncos",
+                color1: 'orange',
+                color2: 'blue'
+            }
         },
         east:{
-            one: "Buffalo Bills",
-            two: "New England Patriots",
-            three: "New York Jets",
-            four: "Miami Dolphins"
+            one: {
+                name: "Buffalo Bills",
+                color1: 'blue',
+                color2: 'red'
+            },
+            two: {
+                name: "New England Patriots",
+                color1: 'red',
+                color2: 'blue'
+            },
+            three: {
+                name: "New York Jets",
+                color1: 'green',
+                color2: 'white'
+            },
+            four: {
+                name: "Miami Dolphins",
+                color1: 'green',
+                color2: 'orange'
+            }
         },
         north:{
-            one: "Pittsburgh Steelers",
-            two: "Baltimore Ravens",
-            three: "Cleveland Browns",
-            four: "Cincinnati Bengals"
+            one: {
+                name: "Pittsburgh Steelers",
+                color1: 'black',
+                color2: 'yellow'
+            },
+            two: {
+                name: "Baltimore Ravens",
+                color1: 'white',
+                color2: 'purple'
+            },
+            three: {
+                name: "Cleveland Browns",
+                color1: 'white',
+                color2: 'orange'
+            },
+            four: {
+                name: "Cincinnati Bengals",
+                color1: 'black',
+                color2: 'orange'
+            }
         }
     },
 }
--- src/pages/Standings.jsx
+++ src/pages/Standings.jsx
@@ -1,16 +1,74 @@
 import React, {useContext} from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, SectionList } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 import {Context} from '../context/authContext';
+import {StandingsStyle} from '../styles/Standings';
 
 const Standings = ({navigation})=>{
 
     const {details} = useContext(Context);
 
     return(
-        <View>
-            <Text>{details.nfc.west.one}</Text>
-            <Text>{details.nfc.west.two}</Text>
+        <View style={StandingsStyle.main}>
+            <View style={StandingsStyle.topWordView}><Text style={StandingsStyle.topWordText}>Divisional Standings</Text></View>
+            <View style={StandingsStyle.list}>
+                <SectionList
+                sections={[
+                    {title: 'NFC West', data: [
+                        {key: details.nfc.west.one.name, color1: details.nfc.west.one.color1, color2: details.nfc.west.one.color2},
+                        {key: details.nfc.west.two.name, color1: details.nfc.west.two.color1, color2: details.nfc.west.two.color2},
+                        {key: details.nfc.west.three.name, color1: details.nfc.west.three.color1, color2: details.nfc.west.three.color2},
+                        {key: details.nfc.west.four.name, color1: details.nfc.west.four.color1, color2: details.nfc.west.four.color2}]},
+                    {title: 'NFC South', data: [
+                        {key: details.nfc.south.one.name, color1: details.nfc.south.one.color1, color2: details.nfc.south.one.color2},
+                        {key: details.nfc.south.two.name, color1: details.nfc.south.two.color1, color2: details.nfc.south.two.color2},
+                        {key: details.nfc.south.three.name, color1: details.nfc.south.three.color1, color2: details.nfc.south.three.color2},
+                        {key: details.nfc.south.four.name, color1: details.nfc.south.four.color1, color2: details.nfc.south.four.color2}
+                    ]},
+                    {title: 'NFC North', data: [
+                        {key: details.nfc.north.one.name, color1: details.nfc.north.one.color1, color2: details.nfc.north.one.color2},
+                        {key: details.nfc.north.two.name, color1: details.nfc.north.two.color1, color2: details.nfc.north.two.color2},
+                        {key: details.nfc.north.three.name, color1: details.nfc.north.three.color1, color2: details.nfc.north.three.color2},
+                        {key: details.nfc.north.four.name ,color1: details.nfc.north.four.color1, color2: details.nfc.north.four.color2}
+                    ]},
+                    {title: 'NFC East', data: [
+                        {key: details.nfc.east.one.name, color1: details.nfc.east.one.color1, color2: details.nfc.east.one.color2},
+                        {key: details.nfc.east.two.name, color1: details.nfc.east.two.color1, color2: details.nfc.east.two.color2},
+                        {key: details.nfc.east.three.name, color1: details.nfc.east.three.color1, color2: details.nfc.east.three.color2},
+                        {key: details.nfc.east.four.name, color1: details.nfc.east.four.color1, color2: details.nfc.east.four.color2}
+                    ]},
+
+                    {title: 'AFC West', data: [
+                        {key: details.afc.west.one.name, color1: details.afc.west.one.color1, color2: details.afc.west.one.color2},
+                        {key: details.afc.west.two.name, color1: details.afc.west.two.color1, color2: details.afc.west.two.color2},
+                        {key: details.afc.west.three.name, color1: details.afc.west.three.color1, color2: details.afc.west.three.color2},
+                        {key: details.afc.west.four.name, color1: details.afc.west.four.color1, color2: details.afc.west.four.color2}
+                    ]},
+                    {title: 'AFC South', data: [
+                        {key: details.afc.south.one.name, color1: details.afc.south.one.color1, color2: details.afc.south.one.color2},
+                        {key: details.afc.south.two.name, color1: details.afc.south.two.color1, color2: details.afc.south.two.color2},
+                        {key: details.afc.south.three.name, color1: details.afc.south.three.color1, color2: details.afc.south.three.color2},
+                        {key: details.afc.south.four.name, color1: details.afc.south.four.color1, color2: details.afc.south.four.color2}
+                    ]},
+                    {title: 'AFC North', data: [
+                        {key: details.afc.north.one.name, color1: details.afc.north.one.color1, color2: details.afc.north.one.color2},
+                        {key: details.afc.north.two.name, color1: details.afc.north.two.color1, color2: details.afc.north.two.color2},
+                        {key: details.afc.north.three.name, color1: details.afc.north.three.color1, color2: details.afc.north.three.color2},
+                        {key: details.afc.north.four.name, color1: details.afc.north.four.color1, color2: details.afc.north.four.color2}
+                    ]},
+                    {title: 'AFC East', data: [
+                        {key: details.afc.east.one.name, color1: details.afc.east.one.color1, color2: details.afc.east.one.color2},
+                        {key: details.afc.east.two.name, color1: details.afc.east.two.color1, color2: details.afc.east.two.color2},
+                        {key: details.afc.east.three.name, color1: details.afc.east.three.color1, color2: details.afc.east.three.color2},
+                        {key: details.afc.east.four.name, color1: details.afc.east.four.color1, color2: details.afc.east.four.color2}
+                    ]}
+                ]}
+                renderItem={({item}) => <TouchableOpacity style={{marginTop: 5, backgroundColor: item.color2}}><Text style={StandingsStyle.teamText, {fontSize: 18, fontWeight: 'bold', color: item.color1}}>{item.key}</Text></TouchableOpacity>}
+                renderSectionHeader={({section}) => <Text style={section.title[0] === 'N'? StandingsStyle.nfc: StandingsStyle.afc}>{section.title}</Text>}
+                keyExtractor={(item, index) => index}
+                />
+            </View>
         </View>
     )
 }
